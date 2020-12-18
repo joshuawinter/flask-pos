5112ac351f22ad9128fda866d01efbd05aadb21b
josh parsons <jwparsons@google.com>
2020-12-18 17:21:28 +0000

    first working

--- pos/config.py
+++ pos/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:bigben00@gofish.cd7znfetkbhy.us-west-2.rds.amazonaws.com:3306/gofish'
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:bigben00@10.15.32.3:3306/gofish'
     #'mysql://root:bigben00@localhost/pos'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
